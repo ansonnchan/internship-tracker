b8a469f6c9eb29079de58ccecf64a4397dd004a2
ansonnchan <ananryry180@gmail.com>
2026-02-14 22:25:34 -0800

    models>application.java

---
+++ internshiptracker/src/main/java/com/anson/internshiptracker/model/Application.java
@@ -0,0 +1,23 @@
+package com.anson.internshiptracker.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "applications")
+@Data
+public class Application {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String company;
+    private String position;
+    private String status; // APPLIED, INTERVIEW, OFFER, REJECTED, OA
+    private LocalDate dateApplied;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id") 
+    private User user; 
+}
